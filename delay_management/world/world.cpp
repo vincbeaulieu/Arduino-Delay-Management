604918a0e1d98723dea5a8f718b1962d796d205d
Vincent Beaulieu <37360448+vincbeaulieu@users.noreply.github.com>
2023-09-14 13:58:31 -0400

    Update world.cpp

--- delay_management/world/world.cpp
+++ delay_management/world/world.cpp
@@ -1,6 +1,5 @@
 //
 //  world.cpp
-//  test
 //
 //  Created by Vincent Beaulieu on 2022-11-26.
 //
@@ -8,8 +7,8 @@
 #include "world.h"
 
 string world(double stuff){
-    // Points/Refers to the previous time assigned to print (PREVIOUS_TIMES.print)
-    volatile unsigned long *previous_time = &PREVIOUS_TIMES.print;
+    // Points/Refers to the previous time assigned to "world" (PREVIOUS_TIMES.world)
+    volatile unsigned long *previous_time = &PREVIOUS_TIMES.world;
     
     // Set your delay
     unsigned long delay = 1000;
@@ -20,7 +19,7 @@ string world(double stuff){
         // Place your code here
         printf("\t %.2f\n", stuff);
         
-        // Update PREVIOUS_TIMES.print to current_time
+        // Update PREVIOUS_TIMES.world to current_time
         *previous_time = millis();
     }
     
